d840b7f467fd594d3b198268a7ac5fe887b242a5
Christiaan van Tienhoven <tienhoven.c@tkppensioen.nl>
2018-02-01 16:05:43 +0100

    Version bump

--- src/main/java/org/graylog/TopdeskAlarmCallbackMetaData.java
+++ src/main/java/org/graylog/TopdeskAlarmCallbackMetaData.java
@@ -34,7 +34,7 @@ public class TopdeskAlarmCallbackMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 0, 1);
+        return new Version(1, 0, 2);
     }
 
     @Override
@@ -44,7 +44,7 @@ public class TopdeskAlarmCallbackMetaData implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(2, 0, 0);
+        return new Version(2, 3, 0);
     }
 
     @Override
